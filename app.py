115241097d19788f3c61fe8ef2163bb9ec7d62af
Alex-gikungu <alexigikungu.012@gmail.com>
2025-02-28 00:37:34 +0300

    added cors restrictions

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 # app.py
 from flask import Flask
+from flask_cors import CORS  # Import CORS
 from models import db
 from routes import init_mail, register_routes
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
